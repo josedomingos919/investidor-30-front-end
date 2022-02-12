b94707aa478e470373ddf93e7122413663d9f03d
josedomingos919 <josedomingos919@gmail.com>
2022-02-12 22:48:13 +0100

    adding styled-component

---
+++ src/style/globas.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+ *{
+    padding: 0px;
+    margin: 0px;
+    outline: none;
+    text-decoration: none;
+    box-sizing: border-box;
+ }
+`;
