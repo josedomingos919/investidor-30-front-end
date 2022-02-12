235cf54c1ea08ce35715f357838913d4cf206497
josedomingos919 <josedomingos919@gmail.com>
2022-02-12 23:37:19 +0100

    adding logo and information in login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,30 @@
-import { BodyTopContainer } from "./styles";
+import {
+  Image,
+  ContainerLogo,
+  BodyTopContainer,
+  ContainerFirstInfo,
+  LoginTitle,
+  Paragraph,
+} from "./styles";
+
+import { images } from "./util";
 
 export const Login = () => {
   return (
     <>
-      <BodyTopContainer>Hello</BodyTopContainer>
+      <BodyTopContainer>
+        <ContainerLogo>
+          <Image src={images.Logo} />
+        </ContainerLogo>
+
+        <ContainerFirstInfo>
+          <LoginTitle>Login</LoginTitle>
+          <Paragraph>
+            Área reservada para clientes especiais! se ainda não possui uma
+            conta, solicite o seu agente!
+          </Paragraph>
+        </ContainerFirstInfo>
+      </BodyTopContainer>
     </>
   );
 };
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -6,4 +6,40 @@ export const BodyTopContainer = styled.div`
   background-color: ${colors.primary};
   min-height: 200px;
   color: ${colors.white};
+  padding: 25px 67px;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
+
+export const ContainerLogo = styled.div`
+  text-align: center;
+  width: 100%;
+  margin-bottom: 60px;
+`;
+
+export const ContainerFirstInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  width: 100%;
+  margin-bottom: 30px;
+`;
+
+export const LoginTitle = styled.label`
+  font-weight: bold;
+  color: ${colors.white};
+  margin-bottom: 16px;
+  font-size: 23px;
+`;
+
+export const Paragraph = styled.label`
+  color: ${colors.backgroundSubTitle};
+  font-size: 11px;
+  line-height: 0.98rem;
+  text-align: justify;
+`;
+
+export const Image = styled.img``;
